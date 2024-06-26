36475e7a78ddfd8df4517ae143ac0bd40a4d9168
sarahlynnx <xsarahlynn06x@gmail.com>
2024-06-25 17:19:49 -0700

    Added FB icon, prettier for formatting

--- contact.js
+++ contact.js
@@ -1,8 +1,8 @@
-window.initMap = async function() {
+window.initMap = async function () {
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  const position = { lat: 46.476600281534814, lng: -93.96432872837795 }; 
+  const position = { lat: 46.476600281534814, lng: -93.96432872837795 };
 
   const map = new Map(document.getElementById("map"), {
     zoom: 16,
@@ -11,27 +11,27 @@ window.initMap = async function() {
   });
 
   const marker = new AdvancedMarkerElement({
-      map: map,
-      position: position,
-      title: "Playful Pathways Therapy",  
+    map: map,
+    position: position,
+    title: "Playful Pathways Therapy",
   });
 };
 
 async function loadGoogleMapsAPI() {
   try {
-      const response = await fetch('https://blog-server-8yo5.onrender.com/api/maps-config');
-      const data = await response.json();
-      const script = document.createElement('script');
-      script.src = data.mapsURL;
-      document.head.appendChild(script);
-      script.onload = () => {
-          console.log("Google Maps loaded successfully");
-      };
-      script.onerror = () => {
-          console.error("Error loading Google Maps");
-      };
+    const response = await fetch('https://blog-server-8yo5.onrender.com/api/maps-config');
+    const data = await response.json();
+    const script = document.createElement('script');
+    script.src = data.mapsURL;
+    document.head.appendChild(script);
+    script.onload = () => {
+      console.log("Google Maps loaded successfully");
+    };
+    script.onerror = () => {
+      console.error("Error loading Google Maps");
+    };
   } catch (error) {
-      console.error("Failed to load Google Maps config:", error);
+    console.error("Failed to load Google Maps config:", error);
   }
 }
 
